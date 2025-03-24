933a544f7061d081735994db85906cc5ba7b93ed
Rodri <r.serranvives@gmail.com>
2025-03-24 12:24:47 +0100

    feat: api routes for data fetching on github

---
+++ app/api/github_repos/route.ts
@@ -0,0 +1,23 @@
+import { NextResponse } from "next/server"
+import { Octokit } from "octokit"
+
+export async function GET() {
+    const GITHUB_API_KEY_CLASIC_TOKEN = process.env.GITHUB_API_KEY_CLASIC_TOKEN
+    if (!GITHUB_API_KEY_CLASIC_TOKEN) return (NextResponse.json({ error: "Mising API token" }, { status: 401 }))
+
+    const octokit = new Octokit({
+        auth: GITHUB_API_KEY_CLASIC_TOKEN,
+    })
+    const { data } = await octokit.request('GET /users/{username}/repos',
+        {
+            username: 'rsvives',
+            headers: {
+                'X-GitHub-Api-Version': '2022-11-28',
+                accept: 'application/vnd.github+json'
+            },
+            per_page: 100,
+        })
+
+
+    return NextResponse.json(data)
+}
