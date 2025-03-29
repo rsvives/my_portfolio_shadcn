debeb002c6ad74f35d7facc327eada1fbbb6cf61
Rodri <r.serranvives@gmail.com>
2025-03-29 11:50:19 +0100

    refactor: added title

--- components/FixedStats.tsx
+++ components/FixedStats.tsx
@@ -5,11 +5,17 @@ import CardPullRequestsChart from "./CardPullRequestsChart";
 
 export default async function FixedStats() {
     return (
-        <div id="fixed-stats" className="grid gap-4 mt-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
-            <CardCommitsChart />
-            <CardLanguagesChart />
-            <CardForkedReposChart />
-            <CardPullRequestsChart />
-        </div>
+        <section>
+            <div className="flex items-baseline my-4 gap-2">
+                <h2 className="text-xl font-bold tracking-tight">Github Activity</h2>
+                <span className="text">Latest 90 days</span>
+            </div>
+            <div id="fixed-stats" className="grid gap-4 mt-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
+                <CardCommitsChart />
+                <CardLanguagesChart />
+                <CardForkedReposChart />
+                <CardPullRequestsChart />
+            </div>
+        </section>
     )
 }
