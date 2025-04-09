838d43afe5ec4e8a4263514ce3e2e2495e7d6be6
Rodri <r.serranvives@gmail.com>
2025-04-09 11:30:13 +0200

    fix: minor UI alignment issues

--- components/ProjectListItem.tsx
+++ components/ProjectListItem.tsx
@@ -13,13 +13,13 @@ export function ProjectListItem({ project, showLinks = false }: props) {
     // return (JSON.stringify(project))
 
     return (
-        <Link href={`/projects/${sluggify(project.name)}`} className="flex items-center gap-4 select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground w-[100%]">
+        <Link href={`/projects/${sluggify(project.name)}`} className="flex items-start gap-4 select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground w-[100%]">
             {/* <div key={project.name} className=""> */}
             {/* <Avatar className="self-start hidden sm:block">
                 <AvatarImage src={project.avatar} alt={project.name} />
                 <AvatarFallback></AvatarFallback>
             </Avatar> */}
-            <div className="flex flex-col flex-1 ">
+            <div className="flex flex-col flex-1 gap-2">
                 <h4 className="text-md font-medium">{project.name}</h4>
                 <p className="text-sm text-muted-foreground line-clamp-2">{project.description}</p>
                 <div className="flex flex-wrap gap-1 mt-2">
