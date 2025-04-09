74c2e4a81cf0d9219d83b5eb04fa05eac85df616
Rodri <r.serranvives@gmail.com>
2025-04-09 11:24:48 +0200

    fix: changed CV link

--- app/page.tsx
+++ app/page.tsx
@@ -26,7 +26,7 @@ export default function Home() {
 
   // const description = "I am a product design engineer turned into web developer. I used to design physical products for additive manufacturing but my passion for human centered design made me pivot towards digital product design and development. This passion has motivated me not only to code, but also to teach others to code."
   const description = "I'm a Design Engineer turned Full-Stack Developer. I started my career developing educational robotics projects and designing parts for additive manufacturing, but since 2019, my passion for creating has led me to "
-
+  const CVLink = "https://drive.google.com/file/d/1sqXD06Y9FWIXYvQTtUyzDs_layfSxSu2/view?usp=sharing"
 
   return (
     <div className="grid grid-rows-[1fr_24px] items-center justify-items-center pb-16 gap-20 sm:gap-32">
@@ -49,7 +49,7 @@ export default function Home() {
 
         </section>
         <div className="flex gap-4 items-center flex-row">
-          <Button variant={'outline'} asChild><a href="https://drive.google.com/file/d/1vBcJYj-MCIHrbdUDKRZuOReR2YbGwlo4/view?usp=sharing" target="_blank">Download CV <DownloadIcon /></a></Button>
+          <Button variant={'outline'} asChild><a href={CVLink} target="_blank">Download CV <DownloadIcon /></a></Button>
           <Button>Learn more </Button>
         </div>
 
