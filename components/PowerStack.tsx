a345e15713439940b657ab7fcbf1ce2eece5c8a1
Rodri <r.serranvives@gmail.com>
2025-07-25 16:29:29 +0200

    refactor: moved powerstack to component

---
+++ components/PowerStack.tsx
@@ -0,0 +1,60 @@
+import Image from "next/image"
+import { Card, CardContent } from "./ui/card"
+import { Tooltip, TooltipContent, TooltipTrigger } from "./ui/tooltip"
+
+export function PowerStack() {
+    const powerStack = [
+        {
+            name: 'React',
+            icon: '/logos/react.svg',
+            shadow: 'shadow-none hover:shadow-[0px_0px_12px_0px_#58C4DC]'
+        },
+        {
+            name: 'Javascript',
+            icon: '/logos/javascript.svg',
+            shadow: 'shadow-none hover:shadow-[0px_0px_12px_0px_#F7DF1F]'
+        },
+        {
+            name: 'Nodejs',
+            icon: '/logos/node.svg',
+            shadow: 'shadow-none hover:shadow-[0px_0px_12px_0px_#60A04E]'
+        },
+        {
+            name: 'Express',
+            icon: '/logos/express.svg',
+            shadow: 'shadow-none hover:shadow-[0px_0px_12px_0px_#00000066]'
+        },
+        {
+            name: 'TanStack Query',
+            icon: '/logos/tanstack.png',
+            shadow: 'shadow-none hover:shadow-[0px_0px_12px_0px_#FFC1AF]'
+        },
+        {
+            name: 'MongoDB',
+            icon: '/logos/mongo.svg',
+            shadow: 'shadow-none hover:shadow-[0px_0px_12px_0px_#00ED64]'
+        },
+    ]
+    return (
+        <Card className="">
+            <CardContent className="flex gap-2 items-center justify-between">
+                <h3 className="text-xl font-bold tracking-tight">Power Stack</h3>
+                <div className="flex gap-4">
+                    {powerStack.map(tech =>
+                        <Tooltip key={tech.name}>
+                            <TooltipTrigger asChild >
+                                <Card className={`p-2 w-fit flex-row items-center ${tech.shadow}`}>
+                                    <Image className="aspect-square rounded-md" width={48} height={48} src={tech.icon} alt={`${tech.name} icon`}></Image>
+                                </Card>
+                            </TooltipTrigger>
+                            <TooltipContent>
+                                <p>{tech.name}</p>
+                            </TooltipContent>
+
+                        </Tooltip>
+
+                    )}
+                </div>
+            </CardContent>
+        </Card>)
+}
